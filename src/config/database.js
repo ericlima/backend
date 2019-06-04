302d78f9af2c92780a1e086d5af4a6d4e56e5cfc
Eric Lima <egvlima@impresa.pt>
2019-06-04 03:53:34 +0100

    commit

--- src/config/database.js
+++ src/config/database.js
@@ -4,25 +4,5 @@ const mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise
 
-let options = {    
-    server: {    
-      auto_reconnect: true,    
-      socketOptions: {
-        keepAlive: 1,    
-        connectTimeoutMS: 60000,    
-        socketTimeoutMS: 60000,    
-      }    
-    },    
-    replset: {    
-      auto_reconnect: true,    
-      socketOptions: {    
-        keepAlive: 1,    
-        connectTimeoutMS: 60000,    
-       socketTimeoutMS: 60000,    
-      }    
-   }
-
-  }
-
-module.exports = mongoose.connect(url, options)
+module.exports = mongoose.connect(url)
 
